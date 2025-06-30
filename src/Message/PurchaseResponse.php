6720f2d18c862acadd759eedf50705df71393551
Elnur Akhundov <elnurxf@gmail.com>
2025-06-30 16:33:35 +0400

    Update PurchaseResponse.php

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -7,6 +7,12 @@ use Omnipay\Common\Message\RedirectResponseInterface;
 
 class PurchaseResponse extends AbstractResponse implements RedirectResponseInterface
 {
+    public function isSuccessful()
+    {
+        // Purchase requests are successful when they redirect to payment gateway
+        return $this->isRedirect();
+    }
+
     public function isRedirect()
     {
         return true;
