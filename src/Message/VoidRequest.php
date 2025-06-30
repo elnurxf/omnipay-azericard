3cd75f8d7ada2eaab73b996adf38a9b9e838cc44
Elnur Akhundov <elnurxf@gmail.com>
2025-06-30 15:45:12 +0400

    Create VoidRequest.php

---
+++ src/Message/VoidRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Omnipay\AzeriCard\Message;
+
+class VoidRequest extends AbstractRequest
+{
+    public function getData()
+    {
+        $timestamp = $this->generateTimestamp();
+        $nonce = $this->generateNonce();
+        $amount = number_format($this->getAmount(), 2, '.', '');
+
+        $data = [
+            'AMOUNT'    => $amount,
+            'CURRENCY'  => 'AZN',
+            'ORDER'     => $this->getTransactionId(),
+            'RRN'       => $this->getParameter('rrn'),
+            'INT_REF'   => $this->getParameter('intRef'),
+            'TERMINAL'  => $this->getParameter('terminalId'),
+            'TRTYPE'    => '24',
+            'TIMESTAMP' => $timestamp,
+            'NONCE'     => $nonce,
+        ];
+
+        $data['P_SIGN'] = $this->sign([
+            $amount,
+            $data['CURRENCY'],
+            $data['TERMINAL'],
+            $data['TRTYPE'],
+            $data['ORDER'],
+            $data['RRN'],
+            $data['INT_REF'],
+        ]);
+
+        return $data;
+    }
+
+    public function sendData($data)
+    {
+        return $this->response = new PurchaseResponse($this, $data);
+    }
+}
